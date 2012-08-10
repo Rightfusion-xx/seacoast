a016cd8e94d5942bab68a0a46af8be2887cded4d
victor <victor.pristupa@rightfusion.com>
2012-08-10 17:47:34 +0300

    add choose price button

--- includes/header_template.php
+++ includes/header_template.php
@@ -1,37 +1,37 @@
+<script type="text/javascript" src="/jquery/jquery-1.3.min.js"></script>
 <div class="container">
    <div class="row">
       <div class="span12">
          <div class="header row-fluid">
           <ul class="span6 left-menu">
-              <li><a href="/brand.php" title="Our Brands" class="relative<?php echo ((stristr($_SERVER['PHP_SELF'], 'brand.php') != false) ? ' active': '')?>"><i class="left"></i><i class="right"></i>Our Brands</a></li>
+              <li><a href="/brand.php" title="Our Brands" class="relative<?php echo ((stristr($_SERVER['PHP_SELF'], 'brand.php') != false) ? ' active': '')?>"><em class="left">&nbsp;</em><em class="right">&nbsp;</em>Our Brands</a></li>
               <?php if(!$_SESSION['cm_is_member']):?>
-                  <li><a href="/community/" title="Join Vitamins-Direct" class="relative<?php echo ((stristr($_SERVER['PHP_SELF'], 'community') != false) ? ' active': '')?>"><i class="left"></i><i class="right"></i>Join Vitamins-Direct</a></li>
+                  <li><a href="/community/" title="Join Vitamins-Direct" class="relative<?php echo ((stristr($_SERVER['PHP_SELF'], 'community') != false) ? ' active': '')?>"><em class="left">&nbsp;</em><em class="right">&nbsp;</em>Join Vitamins-Direct</a></li>
               <?php endif; ?>
-              <li><a href="/shipping.php" title="FAQ & Shipping" class="relative<?php echo ((stristr($_SERVER['PHP_SELF'], 'shipping.php') != false) ? ' active': '')?>"><i class="left"></i><i class="right"></i>FAQ & Shipping</a></li>
+              <li><a href="/shipping.php" title="FAQ & Shipping" class="relative<?php echo ((stristr($_SERVER['PHP_SELF'], 'shipping.php') != false) ? ' active': '')?>"><em class="left">&nbsp;</em><em class="right">&nbsp;</em>FAQ & Shipping</a></li>
           </ul>
           <div class="span6 blue">
               <ul class="right-menu">
-                  <li class="first"><a href="/brand.php" title="Our Brands" class="relative<?php echo ((stristr($_SERVER['PHP_SELF'], 'brand.php') != false) ? ' active': '')?>"><i class="left"></i><i class="right"></i>Our Brands</a></li>
+                  <li class="first"><a href="/brand.php" title="Our Brands" class="relative<?php echo ((stristr($_SERVER['PHP_SELF'], 'brand.php') != false) ? ' active': '')?>"><em class="left">&nbsp;</em><em class="right">&nbsp;</em>Our Brands</a></li>
                   <?php if(!$_SESSION['cm_is_member']):?>
-                  <li class="second"><a href="/community/" title="Join Vitamins-Direct" class="relative<?php echo ((stristr($_SERVER['PHP_SELF'], 'community') != false) ? ' active': '')?>"><i class="left"></i><i class="right"></i>Join Vitamins-Direct</a></li>
+                  <li class="second"><a href="/community/" title="Join Vitamins-Direct" class="relative<?php echo ((stristr($_SERVER['PHP_SELF'], 'community') != false) ? ' active': '')?>"><em class="left">&nbsp;</em><em class="right">&nbsp;</em>Join Vitamins-Direct</a></li>
                   <?php endif; ?>
-                  <li class="last"><a href="/shipping.php" title="FAQ & Shipping" class="relative<?php echo ((stristr($_SERVER['PHP_SELF'], 'shipping.php') != false) ? ' active': '')?>"><i class="left"></i><i class="right"></i>FAQ & Shipping</a></li>
+                  <li class="last"><a href="/shipping.php" title="FAQ & Shipping" class="relative<?php echo ((stristr($_SERVER['PHP_SELF'], 'shipping.php') != false) ? ' active': '')?>"><em class="left">&nbsp;</em><em class="right">&nbsp;</em>FAQ & Shipping</a></li>
               </ul>
               <div class="clear"></div>
-              <span class="country relative"><i></i>800.555.6792</span>
-              <span class="world relative"><i></i>(1+) 702.508.9054</span>
+              <span class="country relative"><em>&nbsp;</em>800.555.6792</span>
+              <span class="world relative"><em>&nbsp;</em>(1+) 702.508.9054</span>
           </div>
           <div class="clear"></div>
       </div>
 
          <div class="user-controls row-fluid">
-
            <div class="span6 relative search big">
               <a href="<?php echo HTTP_SERVER?>" class="logo fl-left" title="Vitamins, Exclusive Discounts, Direct to You."></a>
               <div class="search-container fl-left relative">
                   <form action="/topic.php" method="get" style="display:inline;">
                     <input id="search_input" type="text" name="health" maxlength="250" onFocus="changeSubmit()" onBlur="resetSubmit()" value="<?php echo htmlentities($_REQUEST['health'])?>">
-                    <input type="submit" value="" tytle="Search Seacoast" onclick="this.parentNode.submit();">
+                    <input type="submit" value="" title="Search Seacoast" onclick="this.parentNode.submit();">
                   </form>
                   <script type="text/javascript">
                      function changeSubmit(){
@@ -46,19 +46,20 @@
 
            <div class="span6 controllers big">
               <a title="<?php echo (tep_session_is_registered('customer_id') ? HEADER_TITLE_LOGOFF : HEADER_TITLE_LOGIN)?>" href="<?php echo (tep_session_is_registered('customer_id') ? tep_href_link(FILENAME_LOGOFF, '', 'SSL') : tep_href_link(FILENAME_ACCOUNT, '', 'SSL'))?>" class="log-button fl-right relative">
-                  <i class="left"></i>
-                  <i class="right"></i>
-                  <label></label>
+                  <em class="left">&nbsp;</em>
+                  <em class="right">&nbsp;</em>
+                  <label>&nbsp;</label>
                   <span><?php echo (tep_session_is_registered('customer_id') ? HEADER_TITLE_LOGOFF : HEADER_TITLE_LOGIN)?></span>
               </a>
               <div class="account-control fl-right relative<?php echo (tep_session_is_registered('customer_id') ? ' account-button-on':'')?>">
                   <div class="basket relative fl-right" onclick="return location.href = '<?php if($cart->count_contents() > 0):?> /shopping_cart.php <?php endif;?>'">
-                      <label></label>
+                      <div class="q-tip">&nbsp;</div>
+                      <label>&nbsp;</label>
                       <p class="relative">
-                          <i class="left"></i>
-                          <i class="right"></i>
+                          <em class="left">&nbsp;</em>
+                          <em class="right">&nbsp;</em>
                           <?php if($cart->count_contents() > 0):?>
-                              <a href="/shopping_cart.php">
+                              <a class="q-text" href="/shopping_cart.php">
                                   <span class="items"><?php echo $cart->count_contents()?> <span>item<?php if($cart->count_contents()>1) echo 's';?>:</span></span>
                                   <strong>$<?php echo number_format($cart->show_total(),2);?></strong>
                                   <span>(Savings of <strong>$<?php echo number_format($cart->show_savings(),2); ?></strong>)</span>
@@ -72,9 +73,9 @@
                   </div>
                   <?php if(tep_session_is_registered('customer_id')):?>
                       <a title="Account" class="relative account-button fl-left<?php echo ((stristr($_SERVER['PHP_SELF'], FILENAME_ACCOUNT) != false) ? ' active': '')?>" href="<?php echo tep_href_link(FILENAME_ACCOUNT, '', 'SSL'); ?>">
-                          <i class="left"></i>
-                          <i class="right"></i>
-                          <label></label>
+                          <em class="left">&nbsp;</em>
+                          <em class="right">&nbsp;</em>
+                          <label>&nbsp;</label>
                       </a>
                   <?php endif;?>
               </div>
@@ -86,7 +87,7 @@
                    <div class="search-container fl-left relative<?php echo (tep_session_is_registered('customer_id') ? ' narrow':'')?>">
                        <form action="/topic.php" method="get" style="display:inline;" id="width_change">
                            <input id="search_input_1" type="text" name="health" maxlength="250" onFocus="changeSubmitSmall()" onBlur="resetSubmitSmall()" value="<?php echo htmlentities($_REQUEST['health'])?>">
-                           <input type="submit" value="" tytle="Search Seacoast" onclick="this.parentNode.submit();">
+                           <input type="submit" value="" title="Search Seacoast" onclick="this.parentNode.submit();">
                            <div id="expansion_button" onclick="resizeInput()"></div>
                        </form>
                        <script type="text/javascript">
@@ -104,37 +105,61 @@
                            }
                        </script>
                    </div>
-                   <a href="<?php echo (tep_session_is_registered('customer_id') ? tep_href_link(FILENAME_LOGOFF, '', 'SSL') : tep_href_link(FILENAME_ACCOUNT, '', 'SSL'))?>" class="log-button fl-right relative">
-                       <i class="left"></i>
-                       <i class="right"></i>
-                       <label></label>
+                   <a title="<?php echo (tep_session_is_registered('customer_id') ? HEADER_TITLE_LOGOFF : HEADER_TITLE_LOGIN)?>" href="<?php echo (tep_session_is_registered('customer_id') ? tep_href_link(FILENAME_LOGOFF, '', 'SSL') : tep_href_link(FILENAME_ACCOUNT, '', 'SSL'))?>" class="log-button fl-right relative">
+                       <em class="left">&nbsp;</em>
+                       <em class="right">&nbsp;</em>
+                       <label>&nbsp;</label>
                        <span><?php echo (tep_session_is_registered('customer_id') ? HEADER_TITLE_LOGOFF : HEADER_TITLE_LOGIN)?></span>
                    </a>
                    <div class="account-control fl-right relative<?php echo (tep_session_is_registered('customer_id') ? ' account-button-on':'')?>">
                        <div class="basket relative fl-right" onclick="return location.href = '<?php if($cart->count_contents() > 0):?> /shopping_cart.php <?php endif;?>'">
-                           <label></label>
+                           <label>&nbsp;</label>
+                           <div class="q-tip">&nbsp;</div>
                            <p class="relative">
-                               <i class="left"></i>
-                               <i class="right"></i>
+                               <em class="left">&nbsp;</em>
+                               <em class="right">&nbsp;</em>
                                <?php if($cart->count_contents() > 0):?>
-                               <span class="items"><?php echo $cart->count_contents()?> <span>item<?php if($cart->count_contents()>1) echo 's';?>:</span></span>
-                               <a href="/shopping_cart.php">
-
+                               <a class="items"><?php echo $cart->count_contents()?> <span>item<?php if($cart->count_contents()>1) echo 's';?>:</span></a>
+                               <a class="hover-it" href="/shopping_cart.php">
                                    <strong>$<?php echo number_format($cart->show_total(),2);?></strong>
                                    <span>(Savings of <strong>$<?php echo number_format($cart->show_savings(),2); ?></strong>)</span>
                                </a>
+                               <script type="text/javascript">
+                                   $(document).ready(function(){
+                                       var qTipText = $(".basket p a.q-text").text();
+                                       $(".q-tip").text(qTipText);
+                                       var textWidthWide;
+                                       var textWidthShort = $(".min-width-align .q-tip").width();
+                                       var containerWidth;
+                                       $(".controllers.big .basket, .hover-it").hover(
+                                           function(){
+                                               textWidthWide = $(".controllers.big a.q-text").width();
+                                               containerWidth = $(".controllers.big .basket > p").width();
+                                               console.log(textWidthWide >= containerWidth);
+                                               if(textWidthWide >= containerWidth || textWidthShort){
+                                                   $(".q-tip").css('visibility', 'visible');
+                                               }
+                                           },
+                                           function(){
+                                               if(textWidthWide >= containerWidth || textWidthShort){
+                                                   $(".q-tip").css('visibility', 'hidden');
+                                               }
+                                           }
+                                       );
+                                   });
+                               </script>
                                <?php else:?>
-                                   <a href="javascript:;" title="">
+                                   <a class="empty" href="javascript:;" title="">
                                         <span>Shopping cart empty</span>
                                    </a>
                                <?php endif;?>
                            </p>
                        </div>
                        <?php if(tep_session_is_registered('customer_id')):?>
-                       <a class="relative account-button fl-left<?php echo ((stristr($_SERVER['PHP_SELF'], FILENAME_ACCOUNT) != false) ? ' active': '')?>" href="<?php echo tep_href_link(FILENAME_ACCOUNT, '', 'SSL'); ?>">
-                           <i class="left"></i>
-                           <i class="right"></i>
-                           <label></label>
+                       <a title="Account" class="relative account-button fl-left<?php echo ((stristr($_SERVER['PHP_SELF'], FILENAME_ACCOUNT) != false) ? ' active': '')?>" href="<?php echo tep_href_link(FILENAME_ACCOUNT, '', 'SSL'); ?>">
+                           <em class="left">&nbsp;</em>
+                           <em class="right">&nbsp;</em>
+                           <label>&nbsp;</label>
                        </a>
                        <?php endif;?>
                    </div>
@@ -143,7 +168,7 @@
            <div class="clear"></div>
       </div>
         <div class="content row">
-            <h2 class="span12"><?php echo(!empty($_REQUEST['page_caption']) ? $_REQUEST['page_caption'] :'Vitamins - Direct To Members from Seacoast Vitamins')?></h2>
+            <h1 class="span12"><?php echo(!empty($_REQUEST['page_caption']) ? $_REQUEST['page_caption'] :'Vitamins - Direct To Members from Seacoast Vitamins')?></h1>
         </div>
       </div>
    </div>
