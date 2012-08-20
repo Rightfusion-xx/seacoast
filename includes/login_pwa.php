620487fb9254a6be29525406af39680c794ae2db
victor <victor.pristupa@rightfusion.com>
2012-08-20 12:30:59 +0300

    change authorization page

--- includes/login_pwa.php
+++ includes/login_pwa.php
@@ -12,19 +12,24 @@
                     <td><?php echo tep_draw_separator('pixel_trans.gif', '100%', '10'); ?></td>
                   </tr>
                   <tr>
-                    <td class="main" valign="top">If you are a new customer, please create an account.<p> By creating an account at Seacoast Vitamins you will be able to shop faster, keep track of the orders you have previously made and receive our newsletter as well as product updates.</p></td>
+                    <td class="main" valign="top">
+                        <p>If you are a new customer, please create an account.</p>
+                        <p style="padding-right: 20px;"> By creating an account at Seacoast Vitamins you will be able to shop faster, keep track of the orders you have previously made and receive our newsletter as well as product updates.</p>
+                    </td>
                   </tr>
                   <tr>
                     <td><?php echo tep_draw_separator('pixel_trans.gif', '100%', '10'); ?></td>
                   </tr>
                   <tr>
-                    <td><table border="0" width="100%" cellspacing="0" cellpadding="2">
-                      <tr>
-                        <td width="10"><?php echo tep_draw_separator('pixel_trans.gif', '10', '1'); ?></td>
-                        <td align="right"><?php echo '<a href="' . tep_href_link(FILENAME_CREATE_ACCOUNT, '', 'SSL') . '">' . tep_image_button('button_continue.gif', IMAGE_BUTTON_CONTINUE) . '</a>'; ?></td>
-                        <td width="10"><?php echo tep_draw_separator('pixel_trans.gif', '10', '1'); ?></td>
-                      </tr>
-                    </table></td>
+                    <td style="padding-right: 20px;">
+                        <table border="0" width="100%" cellspacing="0" cellpadding="2">
+                            <tr>
+                                <td width="10"><?php echo tep_draw_separator('pixel_trans.gif', '10', '1'); ?></td>
+                                <td align="right"><?php echo '<a href="' . tep_href_link(FILENAME_CREATE_ACCOUNT, '', 'SSL') . '">' . tep_image_button('button_continue.gif', IMAGE_BUTTON_CONTINUE) . '</a>'; ?></td>
+                                <td width="10"><?php echo tep_draw_separator('pixel_trans.gif', '10', '1'); ?></td>
+                            </tr>
+                        </table>
+                    </td>
                   </tr>
                 </table></td>
               </tr>
@@ -36,7 +41,9 @@
                     <td colspan="2"><?php echo tep_draw_separator('pixel_trans.gif', '100%', '10'); ?></td>
                   </tr>
                   <tr>
-                    <td class="main" colspan="2">If you already have created an account, please log in below</td>
+                    <td class="main" colspan="2">
+                        <p>If you already have created an account, please log in below</p>
+                    </td>
                   </tr>
                   <tr>
                     <td colspan="2"><?php echo tep_draw_separator('pixel_trans.gif', '100%', '10'); ?></td>
@@ -63,7 +70,6 @@
                       <tr>
                         <td width="10"><?php echo tep_draw_separator('pixel_trans.gif', '10', '1'); ?></td>
                         <td align="right"><?php echo tep_image_submit('button_login.gif', IMAGE_BUTTON_LOGIN); ?></td>
-                        <td width="10"><?php echo tep_draw_separator('pixel_trans.gif', '10', '1'); ?></td>
                       </tr>
                     </table></td>
                   </tr>
